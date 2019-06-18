4b1b65e834ae9315c0f2abb865bdc9d761985850
sofhiasouza <49447714+sofhiasouza@users.noreply.github.com>
2019-06-17 21:16:25 -0300

    Create glasnici.cpp

---
+++ COCI/glasnici.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 1e5+10;
+
+double k, vet[MAXN];
+
+int n;
+
+bool work(double x)
+{
+	double f = x+vet[0];
+	for(int i = 1 ; i < n ; i++)
+	{
+		double v = vet[i] - x;
+		if(v-k > f) return false;
+		else
+		{	
+			double aux = f-vet[i];
+			if(aux < 0) aux = 0;
+			f = min(vet[i]+x, f+k);
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	cin >> k;
+	cin >> n;
+
+	for(int i = 0 ; i < n ; i++) cin >> vet[i];
+
+	double ini = 0, fim = 1e9;
+	for(int i = 0 ; i <= 100 ; i++)
+	{
+		double meio = (ini+fim)/2.0;
+		if(work(meio)) fim = meio;
+		else ini = meio;
+	}
+	cout << fixed << setprecision(3);
+	cout << ini << endl;
+}
