c29137158510a55e9e4a3331a302848d95fa4cab
sofhiasouza <49447714+sofhiasouza@users.noreply.github.com>
2019-08-19 14:38:52 -0300

    Create game.cpp

---
+++ POI/2010/game.cpp
@@ -0,0 +1,35 @@
+
+//The solution is the same as in this link:https://github.com/mostafa-saad/MyCompetitiveProgramming/blob/master/Olympiad/POI/official/2010/code/gra/prog/gra.cpp
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAXN 1000000
+
+long long res[MAXN];
+
+int main()
+{
+    int n, k;
+    vector<int> cart;
+
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+    {
+	   scanf("%d", &k);
+	   cart.push_back(k);
+    }
+
+    sort(cart.begin(), cart.end());
+
+    res[0] = cart[0];
+
+   // cout << res[0] << ' ';
+    for(int i=1; i<n; i++)
+	res[i] = max((long long)cart[i] - res[i-1], res[i-1]);//, cout << res[i] << ' ';
+
+    //cout << endl;
+    printf("%lld\n", res[n-1]);
+}
